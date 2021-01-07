6be5a20a5459f4f5ad6deb3de858158eff497d86
ZHAO Xudong <zxdong@gmail.com>
2021-01-07 08:58:18 +0800

    Fix build

--- build/download-adapter.js
+++ build/download-adapter.js
@@ -18,17 +18,19 @@ const {
 const server = GH
   ? RINGCENTRAL_APP_SERVER_GH
   : RINGCENTRAL_APP_SERVER
-
+const path = GH
+  ? 'docs'
+  : 'deploy'
 async function run () {
-  rm('-rf', 'docs/embeddable')
+  rm('-rf', `${path}/embeddable`)
   rm('-rf', 'gh-pages.zip')
   exec('wget https://github.com/ringcentral/engage-voice-embeddable/archive/gh-pages.zip')
   exec('unzip -a gh-pages.zip')
-  cp('-r', 'engage-voice-embeddable-gh-pages', 'docs/embeddable')
+  cp('-r', 'engage-voice-embeddable-gh-pages', `${path}/embeddable`)
   rm('-rf', 'engage-voice-embeddable-gh-pages')
-  rm('docs/embeddable/index.html')
+  rm(`${path}/embeddable/index.html`)
   rm('-rf', 'gh-pages.zip')
-  const htmlPath = resolve(__dirname, '../docs/embeddable/app.html')
+  const htmlPath = resolve(__dirname, `../${path}/embeddable/app.html`)
   let html = readFileSync(htmlPath).toString()
   html = html.replace('<script src="app.js"></script>', `<script src="${server}/ios/platform_www/cordova.js"></script><script src="${server}/js/work.bundle.js"></script>`)
   writeFileSync(htmlPath, html)
--- src/server/app/routes/static.js
+++ src/server/app/routes/static.js
@@ -7,7 +7,7 @@ import { resolve } from 'path'
 export default (app) => {
   const staticPath = resolve(__dirname, '../../dist/static')
   const staticPath1 = resolve(process.cwd(), 'platforms')
-  const staticPath2 = resolve(process.cwd(), 'docs/embeddable')
+  const staticPath2 = resolve(process.cwd(), 'deploy/embeddable')
   app.use(Express.static(staticPath))
   app.use(Express.static(staticPath1))
   app.use('/embeddable', Express.static(staticPath2))
